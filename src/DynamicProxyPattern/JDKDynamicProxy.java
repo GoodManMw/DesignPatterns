f82a404d97f6c7c134444f39ae52d302952b855b
GoodMan <15626404376@163.com>
2018-04-08 11:05:01 +0800

    添加了JDK的动态代理

---
+++ src/DynamicProxyPattern/JDKDynamicProxy.java
@@ -0,0 +1,53 @@
+package DynamicProxyPattern;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class JDKDynamicProxy {
+
+	public static void main(String[] args){
+		
+		RealSubject robj = new RealSubject();
+		InvocationHandler ih = new ProxySubject(robj);
+		ObjInterface pobj = (ObjInterface) Proxy.newProxyInstance(robj.getClass().getClassLoader(), robj.getClass().getInterfaces(), ih);
+		pobj.proxyMethod();
+		
+	}
+	
+}
+
+interface ObjInterface{
+	
+	public void proxyMethod();
+	
+}
+
+class RealSubject implements ObjInterface{
+	
+	@Override
+	public void proxyMethod() {
+		// TODO Auto-generated method stub
+		System.out.println("RealSubject 的实现");
+	}
+	
+}
+
+class ProxySubject implements InvocationHandler{
+	
+	private ObjInterface sub;
+	
+	public ProxySubject(Object obj){
+		this.sub = (ObjInterface)obj;
+	}
+	
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		// TODO Auto-generated method stub
+		System.out.println("代理实现之前的操作");
+		method.invoke(sub, args);
+		System.out.println("代理实现之后的操作");
+		return null;
+	}
+	
+}
